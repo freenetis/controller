5c80562d63799c19a03b80f5d773a578e6888ded
Michal Kliment <quimi@quimi.cz>
2024-09-27 12:49:53 +0200

    Add forgotten changes

--- playbooks/roles/qos/mikrotik/filter_plugins/filter_unidecode.py
+++
@@ -1,9 +0,0 @@
-import unicodedata
-
-def unidecode(str):
-    nfkd_form = unicodedata.normalize('NFKD', str)
-    return nfkd_form.encode('ASCII', 'ignore').decode()
-
-class FilterModule(object):
-    def filters(self):
-        return {'unidecode': unidecode}
